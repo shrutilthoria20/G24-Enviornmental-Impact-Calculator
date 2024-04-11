5fb53a2439ec6d41fd0d5f85373a6efe4dc0c076
shrutilthoria20 <78613849+shrutilthoria20@users.noreply.github.com>
2024-04-11 15:28:38 +0530

    Update page.js

--- app/ChatIcon/page.js
+++ app/ChatIcon/page.js
@@ -1,7 +1,7 @@
 // Chat Icon Component
 import React from 'react';
 
-function ChatIcon({ onClick }) {
+function ChatIcon({ onClick =()=>{}}) {
   return (
     <div className="fixed bottom-0 right-0 mr-8 mb-8 z-50">
   <div className="h-16 w-16 cursor-pointer" onClick={onClick}>
